7040b71d44f58b8cee1ed944614cfff4642c4da0
Andrew Noyes <anoyes34@gmail.com>
2019-11-05 13:08:33 -0800

    Add *_trace binaries, add RandomOrder scheduling strategy

--- fdb-summit-talk/simulation.actor.cpp
+++ fdb-summit-talk/simulation.actor.cpp
@@ -6,6 +6,17 @@
 
 #include "flow/actorcompiler.h" // Must be the last include
 
+#ifdef DO_TRACE
+#define TRACE(...)                                                                                                     \
+	do {                                                                                                               \
+		printf(__VA_ARGS__);                                                                                           \
+	} while (0)
+#else
+#define TRACE(...)                                                                                                     \
+	do {                                                                                                               \
+	} while (0)
+#endif
+
 struct Simulator {
 	virtual Future<Void> delay(double seconds) = 0;
 	virtual double now() = 0;
@@ -55,8 +66,16 @@ private:
 	std::mt19937 rand_;
 };
 
+enum class SchedulingStrategy {
+	InOrder,
+	RandomOrder,
+};
+
 struct RandomSim : Simulator {
-	explicit RandomSim(Random* rand) : rand_(rand) { max_buggified_delay = 0.2 * random01(); }
+	explicit RandomSim(Random* rand, SchedulingStrategy s = SchedulingStrategy::InOrder)
+	  : rand_(rand), scheduling_strategy_(s) {
+		max_buggified_delay = 0.2 * random01();
+	}
 	Future<Void> delay(double seconds) override {
 		if (random01() < 0.25) {
 			seconds += max_buggified_delay * pow(random01(), 1000.0);
@@ -71,10 +90,20 @@ struct RandomSim : Simulator {
 	double random01() override { return rand_->random01(); }
 	void run() override {
 		while (running && !tasks_.empty()) {
-			Task task = std::move(tasks_.front());
-			std::pop_heap(tasks_.begin(), tasks_.end(), std::greater<Task>{});
-			tasks_.pop_back();
-			now_ = task.t;
+			Task task;
+			switch (scheduling_strategy_) {
+			case SchedulingStrategy::InOrder:
+				task = std::move(tasks_.front());
+				std::pop_heap(tasks_.begin(), tasks_.end(), std::greater<Task>{});
+				tasks_.pop_back();
+				break;
+			case SchedulingStrategy::RandomOrder:
+				std::swap(tasks_[randomInt(0, tasks_.size())], tasks_.back());
+				task = std::move(tasks_.back());
+				tasks_.pop_back();
+				break;
+			}
+			now_ = std::max(task.t, now_);
 			task.p.send(Void());
 		}
 	}
@@ -93,6 +122,7 @@ private:
 	double now_ = 0;
 	std::vector<Task> tasks_;
 	Random* rand_; // Not owned
+	SchedulingStrategy scheduling_strategy_;
 	int stable = 0;
 	bool running = true;
 	double max_buggified_delay;
@@ -107,10 +137,11 @@ ACTOR Future<Void> poisson(Simulator* sim, double* last, double meanInterval) {
 struct ExampleService {
 	Future<Void> swap(int i, int j) { return swap_(this, i, j); }
 	Future<Void> checkInvariant() {
+		TRACE("%f\t\t\tcheckInvariant()\n", sim->now());
 		auto copy = elements;
 		std::sort(copy.begin(), copy.end());
 		for (int i = 0; i < copy.size(); ++i) {
-			assert(copy[i] == i);
+			ASSERT_ABORT(copy[i] == i);
 		}
 		return Void();
 	}
@@ -126,11 +157,15 @@ struct ExampleService {
 
 private:
 	ACTOR static Future<Void> swap_(ExampleService* self, int i, int j) {
+		static int freshSwapId = 0;
+		state [[maybe_unused]] int swapId = freshSwapId++;
+		TRACE("%f\t%d\tBegin\tswap(%d, %d)\n", self->sim->now(), swapId, i, j);
 		state int x = self->elements[i];
 		state int y = self->elements[j];
 		wait(self->sim->delay(0)); // This wait is the bug!
 		self->elements[i] = y;
 		self->elements[j] = x;
+		TRACE("%f\t%d\tEnd\tswap(%d, %d)\n", self->sim->now(), swapId, i, j);
 		return Void();
 	}
 
@@ -138,16 +173,21 @@ private:
 	std::vector<int> elements;
 };
 
+std::pair<int, int> sampleDistinctOrderedPair(Simulator* sim, int size) {
+	int i = sim->randomInt(0, size - 1);
+	int j = sim->randomInt(i + 1, size);
+	return { i, j };
+}
+
 ACTOR Future<Void> client(Simulator* sim, ExampleService* service) {
 	state double lastTime = sim->now();
 	loop {
-		wait(poisson(sim, &lastTime, 5));
+		wait(poisson(sim, &lastTime, 1));
 		if (sim->randomInt(0, 100) == 0) {
 			wait(service->checkInvariant());
 		} else {
-			int i = sim->randomInt(0, ExampleService::kSize);
-			int j = sim->randomInt(0, ExampleService::kSize);
-			wait(service->swap(i, j));
+			auto pair = sampleDistinctOrderedPair(sim, ExampleService::kSize);
+			wait(service->swap(pair.first, pair.second));
 		}
 	}
 }
@@ -168,8 +208,9 @@ ACTOR Future<Void> stopAfterSeconds(Simulator* sim, double seconds) {
 }
 
 void runSimulation(Random* random) {
+	TRACE("Time\t\tOpId\tPhase\tOp\n");
 	try {
-		RandomSim sim{ random };
+		RandomSim sim{ random, SchedulingStrategy::RandomOrder };
 		ExampleService service{ &sim };
 		std::vector<Future<Void>> futures;
 		futures.push_back(clients(&sim, &service));
@@ -190,13 +231,19 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
 
 #else
 
-int main() {
+int main(int argc, char** argv) {
+#ifdef DO_TRACE
+	ASSERT(argc > 1);
+	FairRandom rand{ std::atoi(argv[1]) };
+	runSimulation(&rand);
+#else
 	int seed = 0;
 	for (;;) {
 		printf("Trying seed %d\n", seed);
 		FairRandom rand{ seed++ };
 		runSimulation(&rand);
 	}
+#endif
 }
 
 #endif
